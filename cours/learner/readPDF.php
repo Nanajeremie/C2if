658664f33634b50c317a2c4a0be2ebb940e6fdfa
Emmanuel-BIT <52540794+Emmanuel-BIT@users.noreply.github.com>
2021-09-20 13:25:43 +0100

    Update readPDF.php
    remaining tasks:
    - keep the page numbering the database
    - add only courses related to the user
    - add change the current course by the new one on which we have clicked

--- cours/learner/readPDF.php
+++ cours/learner/readPDF.php
@@ -1,3 +1,10 @@
+<?php 
+    include("../../utilities/QueryBuilder.php");
+    $obj = new QueryBuilder();
+    $coursesFollowed = $obj->Select( $table = 'course c, subcription s, learner l', $columns = array(), $status = array('l.MATRICULE'=>'s.MATRICULE', 's.IDCOURSE'=>'c.IDCOURSE'), $orderBy = '', $order = 1);
+    //var_dump($coursesFollowed->fetch());
+?>
+
 <!doctype html>
 <html class="no-js" lang="zxx">
     <head>
@@ -13,8 +20,9 @@
             <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
             <link rel="stylesheet" href="../assets/css/bootstrap.css">
             <link rel="stylesheet" href="../assets/css/fontawesome-all.min.css">
-            <link rel="stylesheet" href="/css/main.css">
-            <script src="https://cdn.jsdelivr.net/npm/pdfjs-dist@2.9.359/build/pdf.min.js"></script>
+            <link rel="stylesheet" href="../assets/css/main.css">
+            <!--<script src="https://cdn.jsdelivr.net/npm/pdfjs-dist@2.9.359/build/pdf.min.js"></script>-->
+            <script src="./build/pdf.js"></script>
     </head>
 
     <body>
@@ -23,7 +31,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-2 bg-dark text-white pt-4">
-                    <div style="position: fixed;">
+                    <div style="">
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-12 col-md-6 mb-3">
@@ -34,7 +42,7 @@
                             </div>
                             <div class="col-12 mt-4">
                                 <span id="page-info">
-                                Pages<span id="init"></span> sur<span id="total"> 6</span>
+                                Pages<span id="init"></span> sur <span id="total">0</span>
                                 </span>
                             </div>
                         </div>
@@ -43,7 +51,7 @@
                                 <h6>Progression de lecture</h6>
                             </div>
                             <div class="p-0 col-9 mt-2 progress border">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width:50%">40%</div>
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" id="progressBar" style="width:0%">0%</div>
                             </div>
                         </div>
                         
@@ -52,7 +60,7 @@
                             <div class="col-12">
                                 <h6>Cours en lecture</h6>
                             </div>
-                        </div>
+                    </div>
                     <div class="container-fluid mt-5">
                         <div class="row">
                         <div class="card col-10 mb-2">
@@ -74,62 +82,39 @@
                 <div class="col-8 bg-secondary">
                     <div class="container mt-4 ">
                         <div class="row justify-content-center">
-                            <canvas id="pdf-render" class="border-danger border" class="col-12"></canvas>
+                            <canvas id="pdf-render" class="border-warning border" class="col-12"></canvas>
                         </div>
                     </div>
                 </div>
+
                 <div class="col-2 bg-dark text-white pt-4 ">
                     <div style="position: fixed; overflow-y: scroll; overflow-x: hidden; max-height:900px" class="ml-4 mb-2">
-                            <div class="col-12 mb-4">
-                                <h5>Vos cours</h5>
-                            </div>
-                            <div class="container ">
-                                <div class="card col-11 mb-2">
-                                    <div class="card-body py-4 text-dark">
-                                        <h5 class="col-12 text-center">Batiments</h5>
-                                        <img src="img/blog-details/1.jpg" width="100%" height="100px" alt="fegfer">
-                                        
-                                        <div class="col-8 justify-content-center mt-2">
-                                            <a href="#" class="btn btn-primary py-0">Lire ce cours</a>
-                                        </div>
+                        <div class="col-12 mb-4">
+                            <h5>Vos cours</h5>
+                        </div>
+                        <div class="container">
+                        <?php while ($course = $coursesFollowed->fetch())
+                            {
+                        ?>
+                            <div class="card col-11 mb-2">
+                                <div class="card-body py-4 text-dark">
+                                    <h5 class="col-12 text-center"><?= $course['COURSETITLE'] ?></h5>
+                                    <img src="../assets/fichier_cours/<?= $course['COURSCOVER']?>" width="100%" height="100px" alt="fegfer">
+                                    
+                                    <div class="col-8 justify-content-center mt-2">
+                                        <a href="#" class="btn btn-primary py-0">Lire ce cours</a>
                                     </div>
-                                </div><div class="card col-11 mb-2">
-                                    <div class="card-body py-4 text-dark">
-                                        <h5 class="col-12 text-center">Batiments</h5>
-                                        <img src="img/blog-details/1.jpg" width="100%" height="100px" alt="fegfer">
-                                        
-                                        <div class="col-8 justify-content-center mt-2">
-                                            <a href="#" class="btn btn-primary py-0">Lire ce cours</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card col-11 mb-2">
-                                    <div class="card-body py-4 text-dark">
-                                        <h5 class="col-12 text-center">Batiments</h5>
-                                        <img src="img/blog-details/1.jpg" width="100%" height="100px" alt="fegfer">
-                                        
-                                        <div class="col-8 justify-content-center mt-2">
-                                            <a href="#" class="btn btn-primary py-0">Lire ce cours</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card col-11 mb-2">
-                                    <div class="card-body py-4 text-dark">
-                                        <h5 class="col-12 text-center">Batiments</h5>
-                                        <img src="img/blog-details/1.jpg" width="100%" height="100px" alt="fegfer">
-                                        
-                                        <div class="col-8 justify-content-center mt-2">
-                                            <a href="#" class="btn btn-primary py-0">Lire ce cours</a>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
+                        <?php }?>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
 		<!-- All JS Custom Plugins Link Here here -->
-        <script src="js/pdfController.js"></script>
+        <script src="./js/pdfController.js"></script>
     </body>
 </html>
